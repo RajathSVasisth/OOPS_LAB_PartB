6dafa2366aaee8ce85b58627a230f94c661fffcc
snitin08 <nitinpandit.2000@gmail.com>
2020-02-28 01:23:55 +0530

    Added sql database but data not going into database but table is created

--- app/src/main/java/com/example/nitinpandit/aicte_activity/Member.java
+++ app/src/main/java/com/example/nitinpandit/aicte_activity/Member.java
@@ -1,6 +1,7 @@
 package com.example.nitinpandit.aicte_activity;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,6 +21,7 @@ import androidx.room.Room;
 public class Member extends AppCompatActivity {
 
 
+    DatabaseHandler db;
     public EditText name;
     public RadioButton male;
     public RadioButton female;
@@ -50,7 +52,7 @@ public class Member extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.member);
-
+         db = new DatabaseHandler(getApplicationContext(),null,null,1);
         name = findViewById(R.id.member_name);
         male =  findViewById(R.id.gender_male);
         female =  findViewById(R.id.gender_female);
@@ -106,23 +108,28 @@ public class Member extends AppCompatActivity {
          member_occupation = occupation.getText().toString();
 
         submit_button = findViewById(R.id.member_submit);
-        DatabaseHandler db = new DatabaseHandler(this);
+
+
 
         Member_info_entity m = new Member_info_entity(member_name,member_gender,member_age,
                 member_occupation, member_phone);
         Log.d("Insert: ", "Inserting ..");
-        db.addMember(m);
+        long i = db.addMember(m);
+        if(i==-1)
+            name.setText("ERROR");
+        else
+            name.setText("SUCCESS");
 
         //Reading
-        List<Member_info_entity> members = db.getAllMembers();
+        /*List<Member_info_entity> members = db.getAllMembers();
 
         for (Member_info_entity mn : members) {
             String log = "Id: " + mn.getID() + " ,Name: " + mn.getName() + " ,Phone: " +
                     mn.getPhone_number();
             // Writing Contacts to log
             Log.d("Name: ", log);
-        }
-        finish();
+        }*/
+        //finish();
     }
 }
 
